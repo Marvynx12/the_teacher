6bb2c9625b15fab08076a8d6d16b9b60ace790fc
Marvynx <maigbiniode@gmail.com>
2022-08-18 14:35:53 +0100

    first

---
+++ App.js
@@ -0,0 +1,14 @@
+class Viosam{
+    constructor (name){
+        this.name = "Ebizimor.near"
+    }
+
+    print(){
+        console.log(this.name)
+    }
+
+
+}
+
+let mar = new Viosam;
+//console.log(mar)
